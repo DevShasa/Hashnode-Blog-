f6d3c18b451707591542170b142a8338b881d74c
Shasa <shasa.thuo@gmail.com>
2020-01-05 21:06:33 +0300

    Added some comments

--- simple_bubble_sort/simple_bubble_sort.py
+++ simple_bubble_sort/simple_bubble_sort.py
@@ -2,11 +2,11 @@
 import random
 import copy
 
-#Use a list to test the bubble sort 
+#Numbers saved into a list which is then sorted through bubble sort  
 #Initialize the list
 unsorted_list = []
 
-#Generate ten random numbers to sort 
+#Generate ten random numbers to sort using the random library, the for loop will generate ten random numbers 
 for count in range(1,11):
     #random.randint generates a random number between 1 and 100
     random_number =  random.randint(1, 100)
@@ -15,6 +15,7 @@ for count in range(1,11):
 #Copy the unsorted list into a new list that is then sorted from high to low using bubble sort 
 list_to_sort = copy.deepcopy(unsorted_list)
 for out_loop_count in range(0, len(list_to_sort)):
+    #Nested for loops allow multiple passes untill list is sorted
     for in_loop_count in range(0, len(list_to_sort)-1):
         #If the left digit in the list is smaller than the right digit switch them 
         if list_to_sort[in_loop_count] < list_to_sort[in_loop_count+1]:
