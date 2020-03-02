8e0eaf5c5093d5bfcc01601c58edc5b57581d0c3
Shasa <shasa.thuo@gmail.com>
2020-03-02 19:53:13 +0300

    made improovements

--- reverse_a_string.py
+++ reverse_a_string.py
@@ -2,20 +2,20 @@
 #Turning it into a function 
 
 def reverse_function(input_string):
-    #Code below goes in here to turn this into a function 
+    #Code below goes in here to turn this into a function
+    unreversed_str = input_string
+    #Get the lengh of the string 
+    strlength =  len(unreversed_str)
 
-#Step one, input the string
-normal_string = input('Enter string here: ')
+    #Reverse the string
+    reversed_str = ""
+    while strlength > 0:
+        reversed_str = reversed_str + unreversed_str[strlength - 1] 
+        strlength = strlength - 1
 
-#Step two, get the lenght of the entered string
-str_length = len(normal_string)
+    return reversed_str
 
-#Use a while loop to reverse the string 
-reverse = ''
-while str_length > 0:
-    reverse = reverse + normal_string[str_length -1]
-    #The loop increments downwards 
-    str_length = str_length -1
-
-print(reverse)
+#Activating the function
+reverse_word = reverse_function("wolan")
+print(reverse_word)
 
